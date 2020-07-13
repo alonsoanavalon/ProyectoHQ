f84e3758f42cf384d58b5310b6477c4da01f3451
keyzen <alonso.anavalon@gmail.com>
2020-07-13 18:02:04 -0400

    formulario contacto 3

--- correo.php
+++ correo.php
@@ -1,15 +1,12 @@
 <?php
-    $destinatario= 'gray.import.export@gmail.com';
-    //Esto es el correo al que se enviará el mensaje.
-    $nombre = $_POST['name'];
-    $asunto = $_POST['asunto'];
-    $mensaje = $_POST['mensaje'];
-    $email = $_POST['email'];
-
-    $header = "Enviado desde HQGray";
-    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
-
-    mail($destinatario, $asunto, $mensajeCompleto, $header);
-    echo "<script>alert('Correo enviado exitosamente')</script>";
-
+        //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
+ $destino = "3lphono@gmail.com";
+    $nombre = $_POST["name"];
+    $email = $_POST["email"];
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nAsunto: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+    echo "<h4>Mensaje enviado exitosamente</h4>"
+//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
 ?>
