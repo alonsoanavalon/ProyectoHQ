cf2283a83a9a2dea5bf3cfc4744f4a2ece11b8fd
keyzen <alonso.anavalon@gmail.com>
2020-07-13 17:28:32 -0400

    nuevo proveedor y href

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario= 'gray.import.export@gmail.com'
+    //Esto es el correo al que se enviará el mensaje.
+    $nombre = $_POST['name'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde HQGray"
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('Correo enviado exitosamente')</script>";
+    echo "<script> set Timeout(\"location.href='index.html'\",1000)</script>";
+
+?>
