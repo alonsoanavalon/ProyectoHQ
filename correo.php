296d45f1e0106ce965cdd0ce58c3c4fb09cca453
keyzen <alonso.anavalon@gmail.com>
2020-07-14 11:47:41 -0400

    formulario

--- correo.php
+++ correo.php
@@ -1,12 +1,58 @@
 <?php
-        //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
- $destino = "3lphono@gmail.com";
-    $nombre = $_POST["name"];
-    $email = $_POST["email"];
-    $asunto = $_POST["asunto"];
-    $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nAsunto: " . $mensaje;
-    mail($destino, "Contacto", $contenido);
-    echo "<h4>Mensaje enviado exitosamente</h4>";
-//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+
+    include("Mailer/src/PHPMailer.php");
+    include("Mailer/src/SMTP.php");
+    include("Mailer/src/Exception.php");
+
+
+    try {
+
+        $emailTo = $_POST["email"]; 
+        $subject = $_POST["asunto"];
+        $bodyEmail = $_POST["mensaje"];
+      
+        $fromemail = "3lphono@gmail.com";
+        $fromname = "3lpho";
+        $host = "gmail.live.com"; // gmail
+        $port = "587";
+        $SMTPAuth = "login";
+        $SMTPSecure = "tls";
+        $password = "elmasmejor0910"; 
+        
+        $mail = new  PHPMailer\PHPMailer\PHPMailer();
+      
+        //Tell PHPMailer to use SMTP
+           $mail->isSMTP();
+           $mail->SMTPDebug = 1;
+           $mail->Host = $host;
+           $mail->Port = $port;
+           $mail->SMTPAuth = $SMTPAuth;
+           $mail->SMTPSecure = $SMTPSecure;
+           $mail->Username = $fromemail;
+           $mail->Password = $password;
+           
+           $mail->setFrom($fromemail, $fromname);
+           $mail->addAddress($emailTo);
+      
+           // asunto
+           $mail->isHTML(true);
+           $mail->Subject = $subject;
+           // cuerpo email
+           $mail->Body = $bodyEmail;
+      
+           if (!$mail->send()) {
+            echo "no se envio"; die();
+           }
+      
+              echo "Correo enviado con exito!!"; die();
+      
+       } catch (Exception $e) {
+        var_dump($e->getMessage());die();
+        
+       }
+
+
+
+
+
 ?>
